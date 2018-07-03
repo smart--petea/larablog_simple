b27c0dd9bac3bd019fa581af298493d7f343463b
petru <smart__petea@mail.ru>
2018-07-03 18:56:57 +0300

    episode 13

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,11 @@
 @extends('main')
 
+@section('stylesheets')
+
+    {!! Html::style('css/parsley.css') !!}
+
+@endsection('stylesheets')
+
 @section('title', '| Create New Post')
 
 @section('content')
@@ -8,15 +14,19 @@
             <h1>Create New Post</h1>
             <hr>
 
-            {!! Form::open(array('route' => 'post.store')) !!}
+            {!! Form::open(array('route' => 'post.store', 'data-parsley-validate' => '')) !!}
                 {{ Form::label('title', 'Title:') }}
-                {{ Form::text('title', null, array('class' => 'form-control')) }}
-
-                {{ Form::label('body', 'Post Body') }}
-                {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }} {{ Form::label('body', 'Post Body') }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
                 {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
             {!! Form::close() !!}
         </div>
     </div>
 @endsection('content')
+
+@section('scripts')
+
+   {!! Html::script('js/parsley.min.js') !!}
+
+@endsection('scripts')
