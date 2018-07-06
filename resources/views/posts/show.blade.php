2b7a510865d2fdb4e828f34384d402b2bc01f369
petru <smart__petea@mail.ru>
2018-07-06 18:42:40 +0300

    episode 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,35 @@
 @section('title', '| View Post')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created At:</dt>
+                    <dd>{{ date('M j, Y H:ia', strtotime($post->created_at)) }}</dd>
+                </dl>
+
+                <dl class="dl-horizontal">
+                    <dt>Last Updated At:</dt>
+                    <dd>{{ date('M j, Y H:ia', strtotime($post->updated_at)) }}</dd>
+                </dl>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('post.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('post.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection('content')
